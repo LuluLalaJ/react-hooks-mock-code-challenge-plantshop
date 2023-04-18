a7763da73bebfeb4288c950dfd2025e5fbbe791f
Chen <chen.jiang.1206@gmail.com>
2023-04-18 12:40:38 -0400

    render plants + add out of stock function

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({plant}) {
+  const { id, name, image ="https://via.placeholder.com/400", price} = plant
+  const [inStock, changeInStock] = useState(true)
+
+  function changeStockStatus() {
+    changeInStock(inStock => !inStock)
+  }
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={"plant name"} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      {inStock ? (
+        <button className="primary" onClick={changeStockStatus}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={changeStockStatus} >Out of Stock</button>
       )}
     </li>
   );
